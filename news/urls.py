7eff7674cfa465414d9e0626f97f88938ce166b9
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-02-19 15:19:39 +0300

    Fixed small errors

--- news/urls.py
+++ news/urls.py
@@ -25,10 +25,20 @@ function as positional arguments
 
 3. Name
 Naming the url allows us to refer to it from elsewhere in our app.
+
+Below we add additional urls to navigate to news according to date
+it was posted.
+
+got this error when didn't put parenthesis around \d{4}-\d{2}-\d{2}
+
+----> past_days_news() missing 1 required positional argument: 'past_date'
+
 """
 urlpatterns = [
     url(r'^$', views.welcome, name='welcome'),
     url(r'^today/$', views.news_of_day, name='newsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',
+        views.past_days_news, name='pastNews'),
 ]
 
 """
--- news/views.py
+++ news/views.py
@@ -3,7 +3,7 @@
 The HttpResponse is a class of the django.http module that is
 responsible for returning a response to the user
 """
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
 
 # Create your views here.
@@ -60,11 +60,39 @@ def convert_dates(dates):
     Function that gets the week-day number for the date.
     I think the day_number returns a number between 0 and 6
     corresponding to the day of the week
+
+    Got and IndexError (list index out of range) because i didn't have Saturday and Sunday in my days list.
     """
     day_number = dt.date.weekday(dates)
-    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    days = ["Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday", "Sunday"]
 
     # Returning the actual day of the week
     day = days[day_number]
 
     return day
+
+
+"""
+Below we define the view function to display news from a past date.
+"""
+
+
+def past_days_news(request, past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+            <html>
+                <body>
+                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+                </body>
+            </html>
+            '''
+    return HttpResponse(html)
