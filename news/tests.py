a84fd7349d143a107f0d7bd1e5f734c18b1a774e
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-02-20 10:09:41 +0300

    Instantiating test passed

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+"""
+Django provides a test module django.test where we import the TestCase class.
 
+We then import our models
+"""
+from django.test import TestCase
+from . models import Editor, Articles, Tags
 # Create your tests here.
+
+
+class EditorTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.test = Editor(first_name='Test',
+                           last_name='Case', email='test@gmail.com')
+
+    # Testing proper instantiation
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test, Editor))
